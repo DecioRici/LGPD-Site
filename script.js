50b05e8c3770162838ff9dae38e29da0531b053d
DecioRici <drn5160@gmail.com>
2024-11-17 21:22:54 -0300

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-document.getElementById('contactForm').addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-  });
-  
-  function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-  }
-  
+// Handle form submission
+document.getElementById('contact-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('Obrigado por entrar em contato! Responderemos em breve.');
+});
